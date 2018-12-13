877c5c1e9e773d16142067a105028edab5c5dc92
Tyler Harris <tharris@osc.edu>
2018-12-13 13:07:51 -0500

    Override status_label helper method from osc_machete_rails to work with Bootstrap 4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+
+  def status_label(job, tag = :span)
+    job ||= OpenStruct.new status: OSC::Machete::Status.not_submitted
+    text = job.status.to_s
+
+    label_class = 'badge-secondary'
+    if job.failed?
+      label_class = 'badge-danger'
+    elsif job.passed?
+      label_class = 'badge-success'
+      text = "Completed"
+    elsif job.active?
+      label_class = 'badge-primary'
+    end
+
+    content_tag tag, class: %I(status-label badge #{label_class}) do
+      text
+    end
+  end
+
 end
